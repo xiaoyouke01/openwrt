045cd0b472d269629c834a89f47951deb7cf7239
free-diy <71208560+free-diy@users.noreply.github.com>
2025-08-26 08:11:49 +0800

    Update immortalwrt_24.10_x86.sh

--- scripts/immortalwrt_24.10_x86.sh
+++ scripts/immortalwrt_24.10_x86.sh
@@ -10,7 +10,7 @@ rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
 #rm -rf feeds/luci/applications/luci-app-netdata
 ##### Git稀疏克隆
 # 参数1是分支名, 参数2是仓库地址, 参数3是子目录，同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开
@@ -23,7 +23,7 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 ##### 更新 golang 1.25 版本
-git clone --depth=1 -b 25.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+#git clone --depth=1 -b 25.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 ##### Themes
 # 拉取argon主题
 git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
