9d4e57086590d056a4287c8aabf18de0b558959a
free-diy <71208560+free-diy@users.noreply.github.com>
2025-08-26 12:32:17 +0800

    Update immortalwrt_24.10_x86.sh

--- scripts/immortalwrt_24.10_x86.sh
+++ scripts/immortalwrt_24.10_x86.sh
@@ -10,7 +10,7 @@ rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/luci-app-passwall
-#rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
 #rm -rf feeds/luci/applications/luci-app-netdata
 ##### Git稀疏克隆
 # 参数1是分支名, 参数2是仓库地址, 参数3是子目录，同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开
@@ -23,7 +23,7 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 ##### 更新 golang 1.25 版本
-#git clone --depth=1 -b 25.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+git clone --depth=1 -b 25.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 ##### Themes
 # 拉取argon主题
 git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
@@ -35,16 +35,18 @@ git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config
 # 添加Lucky
 git clone --depth=1 -b main https://github.com/gdy666/luci-app-lucky package/lucky
 # 添加系统高级设置
-git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
+git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
 # 拉取taskplan定时设置插件
-git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
+git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
 # 设备关机功能
-git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone --depth=1 -b js https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
 # 添加adguardhome,bypass，文件管理助手等
 #luci-app-adguardhome luci-app-homeproxy
 #git_sparse_clone main https://github.com/kenzok8/small-package luci-app-bypass luci-app-fileassistant luci-app-filebrowser luci-app-timecontrol luci-app-control-timewol
 # 添加nikki
 git clone --depth=1 -b main https://github.com/nikkinikki-org/OpenWrt-nikki package/OpenWrt-nikki
+# 添加momo
+git clone --depth=1 -b main https://github.com/nikkinikki-org/OpenWrt-momo package/OpenWrt-momo
 # 添加istore
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
