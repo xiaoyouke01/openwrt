27249774154872769fcd6f05521b8da19aa0c6fe
free-diy <71208560+free-diy@users.noreply.github.com>
2026-03-24 16:05:49 +0800

    Update immortalwrt_25.12_x86.sh

--- scripts/immortalwrt_25.12_x86.sh
+++ scripts/immortalwrt_25.12_x86.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. 修改默认 IP
-sed -i '/lan)/s/192\.168\.[0-9.]*/192.168.100.252/' package/base-files/files/bin/config_generate
+sed -i '/lan)/s/192\.168\.[0-9.]*/192.168.100.253/' package/base-files/files/bin/config_generate
 
 # 2.移除要替换的包
 rm -rf feeds/luci/themes/luci-theme-argon
