130f618910fa1260c1d424c1add73b6f567c1b2a
free-diy <71208560+free-diy@users.noreply.github.com>
2026-04-04 21:43:46 +0800

    Update immortalwrt_24.10_x86.sh

--- scripts/immortalwrt_24.10_x86.sh
+++ scripts/immortalwrt_24.10_x86.sh
@@ -101,7 +101,7 @@ git clone --depth=1 -b main https://github.com/nikkinikki-org/OpenWrt-nikki pack
 #git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 #git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall package/luci-app-passwall
 #git clone --depth=1 -b main https://github.com/Openwrt-Passwall/openwrt-passwall2 package/luci-app-passwall2
-# 添加代理依赖
+# 添加壁虎合集
 git clone --depth=1 -b main https://github.com/free-diy/all-proxy package/all-proxy
 # 添加上网时间控制
 #git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-timecontrol package/luci-app-timecontrol
