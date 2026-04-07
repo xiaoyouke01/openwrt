9fcf185825fd80751a3709af89d241c0df16ed17
free-diy <71208560+free-diy@users.noreply.github.com>
2026-04-07 09:01:40 +0800

    Update openwrt.sh

--- scripts/openwrt.sh
+++ scripts/openwrt.sh
@@ -153,6 +153,4 @@ fi
 
 # 9. 其他
 # 专门针对 advancedplus 的流氓逻辑进行清洗
-if [ -f package/luci-app-advancedplus/root/etc/init.d/advancedplus ]; then
-    sed -i '/zsh/d' package/luci-app-advancedplus/root/etc/init.d/advancedplus
-fi
+[ -f package/luci-app-advancedplus/root/etc/init.d/advancedplus ] && sed -i '/zsh/d' package/luci-app-advancedplus/root/etc/init.d/advancedplus
